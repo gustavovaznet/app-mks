20f5b78d1eae9f3837101f1f89994c5b47e1c89c
Gustavo Vaz <gustavovaznet@gmail.com>
2022-11-15 10:52:09 -0300

    Configure backend files

---
+++ backend/src/main/java/com/gvproject/mksshop/MksshopApplication.java
@@ -0,0 +1,13 @@
+package com.gvproject.mksshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MksshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MksshopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gvproject/mksshop/MksshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvproject.mksshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MksshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
