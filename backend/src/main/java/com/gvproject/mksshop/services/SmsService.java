2d8d3234170be685ec56c365d9c38e2f264c0be3
Gustavo Vaz <gustavovaznet@gmail.com>
2022-11-17 21:03:55 -0300

    Configure sms function

--- backend/src/main/java/com/gvproject/mksshop/services/SmsService.java
+++ backend/src/main/java/com/gvproject/mksshop/services/SmsService.java
@@ -1,8 +1,11 @@
 package com.gvproject.mksshop.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.gvproject.mksshop.entities.Sale;
+import com.gvproject.mksshop.repositories.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -21,15 +24,23 @@ public class SmsService {
 
 	@Value("${twilio.phone.to}")
 	private String twilioPhoneTo;
+	
+	@Autowired
+	private SaleRepository saleRepository;
 
-	public void sendSms() {
-
+	public void sendSms(Long saleId) {
+		Sale sale = saleRepository.findById(saleId).get();
+		
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+		
+		String msg = "The salesman" + sale.getSellerName() + "stood out" + date + "with a total of $" + String.format("%.2f", sale.getAmount());
+		
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
 		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-		Message message = Message.creator(to, from, "Teste").create();
+		Message message = Message.creator(to, from, msg).create();
 
 		System.out.println(message.getSid());
 	}
