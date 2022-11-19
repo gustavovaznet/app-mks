b8e26355fcee4bb139fe85a47e384e4d602b7095
Gustavo Vaz <gustavovaznet@gmail.com>
2022-11-19 12:38:44 -0300

    Add toast message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 //COMPONENTS
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 //APP
 function App() {
   return (
     <>
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
