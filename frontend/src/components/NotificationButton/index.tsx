addcb88b5512d6577a1f2585c53cad6f32c5945b
Gustavo Vaz <gustavovaznet@gmail.com>
2022-11-19 12:33:39 -0300

    Create send notification function

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,24 @@
+import axios from 'axios';
 import notificationImg from '../../assets/img/notification-icon.svg';
-
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id: number){
+  axios(`${BASE_URL}/sales/${id}/notification`)
+  .then(response => {
+    console.log("Message sent successfully!")
+  })
+}
+
 //NOTIFICATION BUTTON COMPONENT
-function NotificationButton() {
+function NotificationButton( {saleId} : Props) {
     return (
       <>
-        <div className="mksshop-red-btn">
+        <div className="mksshop-red-btn" onClick={() => handleClick(saleId)}>
             <img src={notificationImg} alt="Notification" />
         </div>
       </>
